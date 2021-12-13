c0bf51edbaf7a37d3d6da01dcc8de3c4c736ba1b
msemerni <msemerni@gmail.com>
2021-12-13 21:51:43 +0200

    555

--- HW/Lection_14_HW_klass_string_methods/js/main.js
+++ HW/Lection_14_HW_klass_string_methods/js/main.js
@@ -11,7 +11,146 @@
 
 console.log("************** 1st ***************");
 
+const studentArr = [{
+    name: 'Сергей',
+    surname: 'Войлов',
+    ratingPoint: 1000,
+    schoolPoint: 1000,
+    course: 2,
+},
+{
+    name: 'Татьяна',
+    surname: 'Коваленко',
+    ratingPoint: 880,
+    schoolPoint: 700,
+    course: 1,
+},
+{
+    name: 'Анна',
+    surname: 'Кугир',
+    ratingPoint: 1430,
+    schoolPoint: 1200,
+    course: 3,
+},
+{
+    name: 'Станислав',
+    surname: 'Щелоков',
+    ratingPoint: 1130,
+    schoolPoint: 1060,
+    course: 2,
+},
+{
+    name: 'Денис',
+    surname: 'Хрущ',
+    ratingPoint: 1000,
+    schoolPoint: 990,
+    course: 4,
+},
+{
+    name: 'Татьяна',
+    surname: 'Капустник',
+    ratingPoint: 650,
+    schoolPoint: 500,
+    course: 3,
+},
+{
+    name: 'Максим',
+    surname: 'Меженский',
+    ratingPoint: 990,
+    schoolPoint: 1100,
+    course: 1,
+},
+{
+    name: 'Денис',
+    surname: 'Марченко',
+    ratingPoint: 570,
+    schoolPoint: 1300,
+    course: 4,
+},
+{
+    name: 'Антон',
+    surname: 'Завадский',
+    ratingPoint: 1090,
+    schoolPoint: 1010,
+    course: 3
+},
+{
+    name: 'Игорь',
+    surname: 'Куштым',
+    ratingPoint: 870,
+    schoolPoint: 790,
+    course: 1,
+},
+{
+    name: 'Инна',
+    surname: 'Скакунова',
+    ratingPoint: 1560,
+    schoolPoint: 200,
+    course: 2,
+},
+];
 
+let idCounter = 1;
+
+class Student {
+    static listOfStudents = [];
+    constructor(enrollee) {
+        this.id = idCounter++;  ////или array.lenght !!!
+
+        if (sortByRating && enrollee.ratingPoint >= 800) {
+            this.isSelfPayment = false;
+        }
+        else {
+            this.isSelfPayment = true;
+        }
+        
+        Object.entries(enrollee).forEach(([key, value]) => {
+            this[key] = value;
+        })
+        
+        let lOfStud = Student.listOfStudents.push(this);
+
+        function sortByRating (lOfStud) {
+            lOfStud = lOfStud.sort((a, b) => a.ratingPoint - b.ratingPoint);
+            let countOfRating = 0
+            for (let entry of lOfStud) {
+                countOfRating++;
+                if (countOfRating <= lOfStud.length-5 || entry.ratingPoint < 800){
+                    entry.isSelfPayment = true;
+                }
+                else {
+                    entry.isSelfPayment = false;
+                }
+            }
+            // lOfStud = lOfStud.sort((a, b) => a.id - b.id);
+            return lOfStud;
+        };
+        sortByRating(Student.listOfStudents);
+    }
+}
+
+new Student(studentArr[0]);
+console.log(Student.listOfStudents);
+new Student(studentArr[1]);
+console.log(Student.listOfStudents);
+new Student(studentArr[2]);
+console.log(Student.listOfStudents);
+new Student(studentArr[3]);
+console.log(Student.listOfStudents);
+new Student(studentArr[4]);
+console.log(Student.listOfStudents);
+new Student(studentArr[5]);
+console.log(Student.listOfStudents);
+new Student(studentArr[6]);
+console.log(Student.listOfStudents);
+new Student(studentArr[7]);
+console.log(Student.listOfStudents);
+new Student(studentArr[8]);
+console.log(Student.listOfStudents);
+new Student(studentArr[9]);
+console.log(Student.listOfStudents);
+new Student(studentArr[10]);
+console.log(Student.listOfStudents);
 
 
 
