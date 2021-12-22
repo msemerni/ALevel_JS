98edb4f2a53b1fc9179bfa39708c54303f0feabe
msemerni <msemerni@gmail.com>
2021-12-22 19:02:18 +0200

    555

--- HW/Lection_17_HW_DOM/js/main.js
+++ HW/Lection_17_HW_DOM/js/main.js
@@ -1,4 +1,80 @@
-﻿
+﻿let form = document.createElement("form");
+document.body.appendChild(form);
+form.method = "GET";
+form.action = "https://google.com/";
+
+let formName = document.createElement("input");
+formName.type = "text";
+formName.name = "name";
+formName.placeholder = "your name";
+form.appendChild(formName);
+
+let formSurname = document.createElement("input");
+formSurname.type = "text";
+formSurname.name = "surname";
+formSurname.placeholder = "your surname";
+form.appendChild(formSurname);
+
+let formAge = document.createElement("input");
+formAge.type = "number";
+formAge.name = "age";
+formAge.placeholder = "your age";
+form.appendChild(formAge);
+
+let submitBtn = document.createElement("button");
+submitBtn.textContent = "Submit";
+submitBtn.type = "submit";
+document.body.appendChild(submitBtn);
+
+//// ошибки ввода
+let alertName = document.createElement("span"); 
+alertName.innerText = "Incorrect name! Should be > 3 letters";
+document.body.appendChild(alertName);
+alertName.style.display = "none";
+
+let alertSurname = document.createElement("span"); 
+alertSurname.innerText = "Incorrect surname! Should be > 3 letters";
+document.body.appendChild(alertSurname);
+alertSurname.style.display = "none";
+
+let alertAge = document.createElement("span"); 
+alertAge.innerText = "Incorrect age! Should be > 15 and < 80";
+document.body.appendChild(alertAge);
+alertAge.style.display = "none";
+
+//// обработчик онклика и валидация
+submitBtn.onclick = () => {
+
+let isFormValid = true;
+
+    if (!(formName.value.length > 3)) {
+        isFormValid = false;
+        alertName.style.display = "flex";
+    }
+
+    if (!(formSurname.value.length > 3)) {
+        isFormValid = false;
+        alertSurname.style.display = "flex";
+    }
+
+    if (!(formAge.value > 15) || !(formAge.value < 80)){
+        isFormValid = false;
+        alertAge.style.display = "flex";
+    }
+
+    console.log(isFormValid);
+    isFormValid && form.submit();
+};
+
+formName.oninput = (e) => {
+    alertName.style.display = "none";
+}
+formSurname.oninput = (e) => {
+    alertSurname.style.display = "none";
+}
+formAge.oninput = (e) => {
+    alertAge.style.display = "none";
+}
 
 
 
--- HW/Lection_18_HW_DOM/js/main.js
+++ HW/Lection_18_HW_DOM/js/main.js
@@ -11,9 +11,9 @@ function addProduct ({name, imgUrl, id, faceId, price, storage}) {
     b.innerText = "Buy";
     b.classList = "btn btn-primary";
     let a = document.createElement("div");
+
     a.classList = "card";
     let columns;
-
     for (let i = 0; i < columnsRadio.length; i++) {
         if (columnsRadio[i].checked === true) {
             columns = columnsRadio[i].value;
@@ -39,9 +39,11 @@ function addProduct ({name, imgUrl, id, faceId, price, storage}) {
     <input type="checkbox" ${faceId ? "checked" : ""}
     <span>${price}</span>
     `
+    // a.appendChild(b);
     a.append(b);
 
     b.addEventListener("click", (e) => {
+        // storage > 0 ? alert("You bought product with price " + price) : alert("No products");
         if (storage > 0) {
             let spanbasketValue = spanbasket.textContent;
             spanbasket.textContent = ++spanbasketValue;
@@ -50,21 +52,36 @@ function addProduct ({name, imgUrl, id, faceId, price, storage}) {
                     productsInBasket.push(item);
                 }
             })
+            // spanbasket.title = productsInBasket.forEach(item => {spanbasket.title += item.name});
             basketIcon.title += `${name}\n`;
             basketIcon.classList.remove("basket-empty");
             basketIcon.classList.add("basket-full");
             console.log(productsInBasket);
+
         }
         else {
             alert ("No product in stock")
         };
     });
+    // b.addEventListener("click", buyProduct);
     document.getElementById("cardContainer").append(a);
 }
 
-items.map((item) => addProduct(item));
+// let productsInBasket = [];
+// function buyProduct(e) {
+//     let spanbasket = document.getElementById("spanbasket");
+//     let spanbasketValue = spanbasket.textContent;
+//     spanbasket.textContent = ++spanbasketValue;
+//     productsInBasket.push();
 
-////////////////////////////////// find by name //////////////////////////////////////
+// }
+
+
+
+items.map((item) => addProduct(item));
+// addProduct(items[0]);
+
+////////////////////////////////// поиск по имени //////////////////////////////////////
 let searchPanel = document.getElementById ("searchPanel");
 let searchLabel = document.createElement("span");
 searchLabel.innerText = "Search by name:"
@@ -95,8 +112,10 @@ function findByName () {
     }
 }
 
-////////////////////////////////// find by price //////////////////////////////////////
+
+////////////////////////////////// поиск по цене //////////////////////////////////////
 function getMinMaxPrice (items) {
+    // let minPrice = items[0].price;
     let minPrice = Infinity;
     let maxPrice = 0;
 
@@ -109,6 +128,13 @@ function getMinMaxPrice (items) {
         }
     }
     return [minPrice, maxPrice];
+
+    // items.map(item => {
+    //     if (item.price < minPrice) {minPrice = item.price};
+    //     if (item.price > maxPrice) {maxPrice = item.price};
+    // })
+
+    // items.filter (item => (item.price < 1500) && (item.price > 1000));
 }
 
 let [minPrice, maxPrice] = getMinMaxPrice(items);
@@ -143,7 +169,34 @@ findPriceBtn.addEventListener("click", () => {
     }
 });
 
-////////////////////////////////// 3 or 6 //////////////////////////////////////
+
+/////////////// второй вариант панели поиска по цене///////////////////
+// let textFrom = document.createElement("span");
+// textFrom.innerHTML = "from";
+// firstPanel.appendChild(textFrom);
+
+// let minPriceBox = document.createElement("input");
+// minPriceBox.type = "number";
+// minPriceBox.name = "minPrice";
+// minPriceBox.placeholder = minPrice;
+// firstPanel.appendChild(minPriceBox);
+
+// let textTo = document.createElement("span");
+// textTo.innerHTML = "to";
+// firstPanel.appendChild(textTo);
+
+// let maxPriceBox = document.createElement("input");
+// maxPriceBox.type = "number";
+// maxPriceBox.name = "maxPrice";
+// maxPriceBox.placeholder = maxPrice;
+// firstPanel.appendChild(maxPriceBox);
+
+// let fromToFindButton = document.createElement("button");
+// fromToFindButton.textContent = "Find";
+// firstPanel.appendChild(fromToFindButton);
+///////
+
+////////////////////////////////// 3 или 6 //////////////////////////////////////
 document.body.addEventListener ("change", (e) => {
     let card = document.getElementsByClassName("card");
     let target = e.target;
@@ -164,3 +217,46 @@ document.body.addEventListener ("change", (e) => {
             break;
     }
 })
+
+// let radioThree = document.createElement("input");
+// radioThree.type = "radio";
+// radioThree.name="threeOrSix"
+// radioThree.value = "3";
+// radioThree.checked = true;
+// secondPanel.appendChild(radioThree);
+
+// let labelRadioThree = document.createElement("label");
+// labelRadioThree.innerText = "3";
+// labelRadioThree.setAttribute("for", "radioThree")
+// secondPanel.appendChild(labelRadioThree);
+
+// let radioSix = document.createElement("input");
+// radioSix.type = "radio";
+// radioThree.name="threeOrSix"
+// radioSix.value = "6";
+// secondPanel.appendChild(radioSix);
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////
+// function aaa () {
+//     let a = (c = 1)
+// }
+// aaa(); // c = 1;
+
+
+
+
+
+
+
