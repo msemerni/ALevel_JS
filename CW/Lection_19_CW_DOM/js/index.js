5794b26556ba487357cdd4310071ad5324a7ab20
msemerni <msemerni@gmail.com>
2021-12-22 21:43:07 +0200

    555

---
+++ CW/Lection_19_CW_DOM/js/index.js
@@ -0,0 +1,44 @@
+﻿import { users } from "./users.js";
+console.log(users);
+
+// const user = users[0];
+// const {is, address, company, email} = user;
+
+const createCard = (data) => {
+    const {city, suite} = data.address;
+    const card = document.createElement ("div");
+    card.innerHTML = `
+    <div>
+        <h1>Name: ${data.name}</h1>
+        <a href ="mailto: ${data.email}">Email: ${data.email}</h2>
+        <a href="${data.website}">Website</a>
+        <div class="info">
+            <h3>Address: ${city} ${suite}</h3>
+        </div>
+        <button class="show-info">Show info</button>
+        <hr>
+    </div>
+    `
+    const button = card.querySelector(".show-info");
+    const infoElement = card.querySelector(".info");
+    button.onclick = () => {
+        const isShow = infoElement.classList.toggle("show");
+        if(isShow) {
+            button.innerText = "Hide info"
+        }
+        else {
+            button.innerText = "Show info"
+        }
+    }
+    return card;
+}
+
+const renderCards = (list) => {
+    const { body } = document; /// тоже самое что "document.body";
+    const listOfCards = list.map((item) => createCard(item));
+    body.innerHTML = "";
+    body.append (...listOfCards);
+}
+
+renderCards(users);
+
